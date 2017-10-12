968a7605a59936ac5814151d4b69d672b278ba4d
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-10-12 22:03:40 +0900

    コレクションセルの作成

---
+++ CollectionViewSample/SampleCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SampleCollectionViewCell.swift
+//  CollectionViewSample
+//
+//  Created by 水野祥子 on 2017/10/12.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class SampleCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
