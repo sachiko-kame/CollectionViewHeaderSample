3408b72363c6e6a23b34d877c752804261fc4dd0
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-10-12 23:41:56 +0900

    ヘッダーつける

--- CollectionViewSample/ViewController.swift
+++ CollectionViewSample/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController ,UICollectionViewDataSource, UICollection
         super.viewDidLoad()
 
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width:viewWidth, height:viewHieght)
+        layout.itemSize = CGSize(width:80, height:80)
         layout.sectionInset = UIEdgeInsetsMake(16, 10, 60, 10)
         layout.headerReferenceSize = CGSize(width:100,height:30)
         layout.minimumInteritemSpacing = 0.0
@@ -48,6 +48,26 @@ class ViewController: UIViewController ,UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView,layout collectionViewLayout: UICollectionViewLayout,sizeForItemAt indexPath: IndexPath) -> CGSize{
         return CGSize(width: 80, height: 80)
     }
+    
+//    //section
+//    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+//        return 3
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        collectionView.register(reusableViewType: SampleCollectionReusableView.self)
+        let sectionHeaderView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SampleCollectionReusableView", for: indexPath)
+        sectionHeaderView.backgroundColor = UIColor.green
+       return sectionHeaderView
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        referenceSizeForHeaderInSection section: Int) -> CGSize{
+//        return CGSize(width: 400, height: 50)
+//    }
+
 
 
 }
---
+++ CollectionViewSample/parts/SampleCollectionReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleCollectionReusableView.swift
+//  CollectionViewSample
+//
+//  Created by 水野祥子 on 2017/10/12.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class SampleCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
